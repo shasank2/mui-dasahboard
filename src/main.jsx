d7d2e73e290ab1fbe475628f7db1a836164655b1
shasank2 <shasankshakya2@gmail.com>
2023-07-03 12:54:47 +0545

    mui theme fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import './index.css'
 // import './assets/scss/style.scss';
+import themes from './themes';
 import { RouterProvider } from 'react-router-dom'
 import { dashboardRoutes } from './router/dashboardRoutes.jsx'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider, } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <RouterProvider router={dashboardRoutes} />
+    <ThemeProvider theme={themes}>
+      <CssBaseline />
+      <RouterProvider router={dashboardRoutes} />
+    </ThemeProvider>
   </React.StrictMode>,
 )

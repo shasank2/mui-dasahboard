d62133ec71cabc82407cbc37d3aeef3f219176dc
shasank2 <shasankshakya2@gmail.com>
2023-07-17 08:05:54 +0545

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import './assets/scss/style.scss';
 import { RouterProvider } from 'react-router-dom'
 import { dashboardRoutes } from './router/dashboardRoutes.jsx'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './components/Theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <RouterProvider router={dashboardRoutes} />
+    <ThemeProvider theme={theme()}>
+      <CssBaseline />
+      <RouterProvider router={dashboardRoutes} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
